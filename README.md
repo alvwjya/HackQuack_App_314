1a559892cad4a513cf59268fd02ccf39962a7548
MadMonkMash <111272801+MadMonkMash@users.noreply.github.com>
2023-04-18 04:51:37 +1000

    testing git in vscode

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 //Crying is not an emergancy
 
 //Zac is not in charge, he pretends that he is but the main programmer is actually Nabeel
+//Shutup
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 print("hello world")
 
+z = 0
+
 class test:
     x = 500
 
 class person:
-    def __init__(self, age, greetings):
+    def __init__(self, age, greetings): #init is the actual class constructor
         self.age = age
         self.greetings = greetings      
         
@@ -19,5 +21,9 @@ numbers = {10, 4, 6, "bazinga"} #The BAZINGA MOVES????
 
 for i in numbers:
     print(i)
+    
+    
+for i in 5:
+    z += (2*i-5)
 
 print(test.x)
