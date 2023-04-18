da242ec62a326732e54639800ac8c69f6777ff73
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-04-18 20:04:30 +1000

    Update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home/>
+        <SignUp/>
       </header>
     </div>
   );
