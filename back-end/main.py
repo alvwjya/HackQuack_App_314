e57128ae7ea461fdd9f27171e19d9a63c7bb8dbe
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-19 13:36:44 +1000

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -1,2 +1,14 @@
-# BACK END
+from flask import Flask, jsonify, request
 
+
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/", methods=['GET'])
+def hello():
+    return jsonify("Hello World")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
