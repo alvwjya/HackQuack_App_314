8a8fd3f0e8344425341ee6c5cc5a60ea4aafe4db
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-19 15:14:00 +1000

    initilise project

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify, request
-import mysql.connector 
+import mysql.connector
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+print(os.environ.get("DATABASE_URL"))
+
+cnx = mysql.connector.connect(user=os.environ.get("DATABASE_USER"), password=os.environ.get("DATABASE_PASSWORD"),
+                              host=os.environ.get("DATABASE_URL"),
+                              database=os.environ.get("DATABASE_SCHEMA"))
+cnx.close()
 
 
 app = Flask(__name__)
