e4726f6ecc0865b13adec760293d955f14d528b3
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-04-21 19:13:36 +1000

    Update Welcome.jsx

--- front-end/src/pages/Welcome.jsx
+++ front-end/src/pages/Welcome.jsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Image } from "react-bootstrap";
 
 function Welcome() {
   return (
-    <>
-      <Container>
-        <div>Welcome</div>
-        <Button href="/customer-sign-up"variant="primary">Sign up Customer</Button>
-        <Button href="/professional-sign-up" variant="primary">Sign up Professional</Button>
-        <Button href="/login" variant="primary">Log in</Button>
+    <Container>
+      <h1 style={{ display: "flex", justifyContent: "center" }}>Welcome</h1>
+      
+      <Container style={{ display: "flex", justifyContent: "center" }}>
+        <Image
+          src="/favicon.ico"
+          roundedCircle
+          style={{ display: "flex", justifyContent: "center" }}
+        />
       </Container>
-    </>
+
+      <div className="d-grid gap-2">
+        <Button href="/customer-sign-up" variant="primary">
+          Sign up Customer
+        </Button>
+        <Button href="/professional-sign-up" variant="primary">
+          Sign up Professional
+        </Button>
+        <Button href="/login" variant="primary">
+          Log in
+        </Button>
+      </div>
+    </Container>
   );
 }
 
