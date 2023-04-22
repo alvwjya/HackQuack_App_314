8fb9327f0d486701a2d579ab4882550380966e4b
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-23 02:26:46 +1000

    Update test_main.py

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,6 +1,7 @@
-import main
+from .main import create_tables
+from .main import get_business
 
 def test_my_function():
-    main.create_tables()
-    result = main.get_business()
+    create_tables()
+    result = get_business()
     assert len(result) > 0
