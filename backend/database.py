05ac027eed2c3ea1d50b1a67581cbb8f47285406
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-23 02:49:42 +1000

    sad

--- backend/database.py
+++ backend/database.py
@@ -5,15 +5,10 @@ from dotenv import load_dotenv, find_dotenv
 
 
 load_dotenv(find_dotenv())
-# user = os.environ.get("DATABASE_USER")
-# password = os.environ.get("DATABASE_PASSWORD")
-# host = os.environ.get("DATABASE_URL")
-# database = os.environ.get("DATABASE_SCHEMA")
-
-user = os.environ.getenv("DATABASE_USER")
-password = os.environ.getenv("DATABASE_PASSWORD")
-host = os.environ.getenv("DATABASE_URL")
-database = os.environ.getenv("DATABASE_SCHEMA")
+user = os.environ.get("DATABASE_USER")
+password = os.environ.get("DATABASE_PASSWORD")
+host = os.environ.get("DATABASE_URL")
+database = os.environ.get("DATABASE_SCHEMA")
 
 engine = create_engine(
     f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4")
