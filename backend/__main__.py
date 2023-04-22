e061575921347ec05d70a4c94b7d709c96fa1e00
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-22 20:45:58 +1000

    update backend structure

---
+++ backend/__main__.py
@@ -0,0 +1,23 @@
+﻿from models.business import Business
+from database import session_factory
+from datetime import date
+
+
+def createBusiness():
+    session = session_factory()
+    # wollongong = Business("wollongong clean", "1232121", "1234", "north wollongong", "1234567890", "123", date(1984, 10, 20))
+    # session.add(wollongong)
+    session.commit()
+    session.close()
+
+
+def getBusiness():
+    session = session_factory()
+    business_query = session.query(Business)
+    session.close()
+    return business_query
+
+
+if __name__ == "__main__":
+    createBusiness()
+    print(getBusiness())
