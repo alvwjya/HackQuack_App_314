ad2ffcbf4a3b537493420f0b439bde2affb0af12
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-23 01:17:01 +1000

    Update __main__.py

--- backend/__main__.py
+++ backend/__main__.py
@@ -6,17 +6,18 @@ from datetime import date
 
 def create_tables():
     session = session_factory()
-    gong_clean = Business("business_name", "ibn", "password", "location", "card_number", "card_security_num", date(1990, 5, 17))
+    gong_clean = Business("business_name", "ibn", "password", "location",
+                          "card_number", "card_security_num", date(1990, 5, 17))
     session.add(gong_clean)
     session.commit()
     session.close()
+
 
 def get_business():
     session = session_factory()
     business_query = session.query(Business)
     session.close()
     return business_query.all()
-    
 
 
 if __name__ == "__main__":
