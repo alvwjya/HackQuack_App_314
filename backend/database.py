4f3019711cc1295bedb1da3009a4d4fba5741b96
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-04-23 02:44:02 +1000

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -10,10 +10,10 @@ load_dotenv(find_dotenv())
 # host = os.environ.get("DATABASE_URL")
 # database = os.environ.get("DATABASE_SCHEMA")
 
-user = os.environ["DATABASE_USER"]
-password = os.environ["DATABASE_PASSWORD"]
-host = os.environ["DATABASE_URL"]
-database = os.environ["DATABASE_SCHEMA"]
+user = os.environ.getenv("DATABASE_USER")
+password = os.environ.getenv("DATABASE_PASSWORD")
+host = os.environ.getenv("DATABASE_URL")
+database = os.environ.getenv("DATABASE_SCHEMA")
 
 engine = create_engine(
     f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4")
