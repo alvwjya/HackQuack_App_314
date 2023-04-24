1ab1d46ee7a9ede7769688eb8fbca5dd308d3133
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-04-24 18:46:37 +1000

    Update ProfessionalServiceBoard.jsx

--- front-end/src/pages/professional/ProfessionalServiceBoard.jsx
+++ front-end/src/pages/professional/ProfessionalServiceBoard.jsx
@@ -2,32 +2,39 @@ import React from "react";
 import { Container, Form, Card, Button, Row, Col } from "react-bootstrap";
 
 function ProfessionalViewRequest() {
+  const data = [1, 2, 3, 4, 5];
   return (
     <Container className="py-5">
       <h1>SERVICE BOARD</h1>
 
-      <Card>
-        <Card.Header>Service Title</Card.Header>
-        <Card.Body>
-          <Card.Title>Type of Issue</Card.Title>
-          <Card.Subtitle></Card.Subtitle>
-          <Card.Text>Information</Card.Text>
+      <hr />
 
-          <Row>
-            <Col sm={8}>
-              <Button variant="primary">Learn more</Button>
-            </Col>
-            <Col sm={2}>
-              <Button variant="primary">Decline</Button>
-            </Col>
-            <Col sm={2}>
-              <Button variant="primary">Accept</Button>
-            </Col>
-          </Row>
-        </Card.Body>
-        <Card.Footer>Location</Card.Footer>
-        <Card.Footer>Time</Card.Footer>
-      </Card>
+      {data.map((data) => (
+        <Card>
+          <Card.Header>Service Title</Card.Header>
+          <Card.Body>
+            <Card.Title>Type of Issue</Card.Title>
+            <Card.Subtitle></Card.Subtitle>
+            <Card.Text>Information</Card.Text>
+
+            <Row>
+              <Col xs={10}>
+                <Button variant="primary">Learn more</Button>
+              </Col>
+              <Col xs={1}>
+                <Button variant="primary">Decline</Button>
+              </Col>
+              <Col xs={1}>
+                <Button variant="primary">Accept</Button>
+              </Col>
+            </Row>
+          </Card.Body>
+          <Card.Footer>Location</Card.Footer>
+          <Card.Footer>Time</Card.Footer>
+        </Card>
+      ))}
+
+      <hr />
     </Container>
   );
 }
