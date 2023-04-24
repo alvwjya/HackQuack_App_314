2f7fec3696391050efc0455a465b3c8fa5e3da55
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-04-24 17:45:23 +1000

    Update ProfessionalServiceBoard.jsx

--- front-end/src/pages/professional/ProfessionalServiceBoard.jsx
+++ front-end/src/pages/professional/ProfessionalServiceBoard.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form, Card, Button } from "react-bootstrap";
 
 function ProfessionalViewRequest() {
   return (
     <Container className="py-5">
       <h1>SERVICE BOARD</h1>
+
+      <Card>
+        <Card.Header>Service Title</Card.Header>
+        <Card.Body>
+          <Card.Title>Type of Issue</Card.Title>
+          <Card.Subtitle></Card.Subtitle>
+          <Card.Text>Information</Card.Text>
+          <Button variant="primary">Learn more</Button>
+          <Button variant="primary">Decline</Button>
+          <Button variant="primary">Accept</Button>
+        </Card.Body>
+        <Card.Footer>Location</Card.Footer>
+        <Card.Footer>Time</Card.Footer>
+      </Card>
     </Container>
   );
 }
