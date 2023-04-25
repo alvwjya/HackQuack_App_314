8f0ca2f6d850bdd1058b92cbb6ff0e08d21eaa26
MadMonkMash <111272801+MadMonkMash@users.noreply.github.com>
2023-04-25 17:37:17 +1000

    Card Creation
    Made the card, added the elements into it, centred it and added colour to the website

--- front-end/src/pages/Welcome.jsx
+++ front-end/src/pages/Welcome.jsx
@@ -1,35 +1,51 @@
 import React from "react";
-import { Container, Button, Image } from "react-bootstrap";
+import { Container, Button, Image, Card } from "react-bootstrap";
 
 function Welcome() {
   return (
-    <Container>
-      <br/><h1 style={{ display: "flex", justifyContent: "center" }}>Welcome</h1>
+    <Container style={{backgroundColor : "#CDBFA0"}}>
       
-      <Container style={{ display: "flex", justifyContent: "center" }}>
-        <Image
-          src="/newlogo.ico"
-          roundedCircle
-          style={{ display: "flex", justifyContent: "center" }}
-        />
-      </Container>
-
       <br/>
-      <div className="d-grid gap-2">
-        <Button href="/customer-sign-up" variant="primary">
-          Sign up Customer
-        </Button>
-        <Button href="/professional-sign-up" variant="primary">
+
+      <Card className="mx-auto" style={{backgroundColor : "lightslategray", width: 500 }}>
+        <Card.Body>
+          <br/><h1 style={{ display: "flex", justifyContent: "center" }}>Welcome</h1>
+      
+          <Container style={{ display: "flex", justifyContent: "center" }}>
+            <Image
+              src="/newlogo.ico"
+              roundedCircle
+              style={{ display: "flex", justifyContent: "centre" }}
+            />
+          </Container>
+          
+          <br/>
+            
+          <div className="d-grid gap-2">
+          <Button href="/customer-sign-up" variant="primary" >
+             Sign up Customer
+          </Button>
+          
+          <Button href="/professional-sign-up" variant="primary">
           Sign up Professional
-        </Button>
-        <Button href="/login" variant="secondary">
+          </Button>
+        
+          <Button href="/login" variant="secondary">
           Log in
-        </Button>
-      </div>
-      <br/><br/><br/><br/><br/><br/>
+          </Button>
+          
+          </div>
+               
+
+        </Card.Body>
+      </Card>
+
+      <br/><br/><br/><br/><br/><br/> 
+
       <div style={{ display: "flex", justifyContent: "center" }} >HackQuack Enterprises 2023 (TM)</div>
     </Container>
   );
 }
+
 
 export default Welcome;
