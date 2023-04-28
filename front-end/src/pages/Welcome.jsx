1bae011293a721545dec6a9265a8f2eb1aaa88cb
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-04-28 12:24:10 +1000

    Update

--- front-end/src/pages/Welcome.jsx
+++ front-end/src/pages/Welcome.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Container, Button, Image, Card } from "react-bootstrap";
+import { Container, Button, Image, Card, Row, Col } from "react-bootstrap";
 
 function Welcome() {
   return (
-    <div>
+    <div className="background-color:#FBD603">
       <Container className="py-5">
-        <Card className="mx-auto" style={{ width: 500 }}>
+        <Card className="shadow-lg mx-auto" style={{ width: 500 }}>
           <Card.Body>
+            <Row>
+              <Col></Col>
+              <Col></Col>
+            </Row>
             <h1
               style={{
                 display: "flex",
