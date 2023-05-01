976ff4acb1c289f8616528fa2bc14a332460683f
NwayOoKhine <56843388+NwayOoKhine@users.noreply.github.com>
2023-05-01 20:14:40 +1000

    Update rating.py

--- backend/models/rating.py
+++ backend/models/rating.py
@@ -8,18 +8,18 @@ class Rating(Base):
 
     id = Column(Integer, primary_key=True)
     client_id = Column(Integer, ForeignKey('client.id'))
-    business_id = Column(Integer, ForeignKey('business.id'))
+    professional_id = Column(Integer, ForeignKey('professional.id'))
     rating = Column(Float)
     review = Column(String(1000))
     client = relationship("Client", back_populates="ratings")
-    business = relationship("Business", back_populates="ratings")
+    professional = relationship("Professional", back_populates="ratings")
 
-    def __init__(self, client_id, business_id, rating, review):
+    def __init__(self, client_id, professional_id, rating, review):
         self.client_id = client_id
-        self.business_id = business_id
+        self.professional_id = professional_id
         self.rating = rating
         self.review = review
 
     def __repr__(self) -> str:
-        return f"id={self.id}, client_id={self.client_id}, business_id={self.business_id}, rating={self.rating}, review={self.review}"
+        return f"id={self.id}, client_id={self.client_id}, professional_id={self.professional_id}, rating={self.rating}, review={self.review}"
 
