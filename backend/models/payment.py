589a6a6babff4601b1c2ec3012e3d7b46e5e5396
MadMonkMash <111272801+MadMonkMash@users.noreply.github.com>
2023-05-06 10:45:06 +1000

    Created payment file and table
    Creates a SQL table then initialises it depending on if the payment method is a subscription payment or a transaction payment

---
+++ backend/models/payment.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, Float, DateTime, ForeignKey
+from datetime import datetime
+from database import Base
+from sqlalchemy.orm import relationship
+    
+# Creates a table to save a payment transaction
+class Payment:
+    __tablename__ = "payment"
+    
+    payment_id = Column(Integer, primary_key=True)
+    client_id = Column(Integer, ForeignKey('client.id'))
+    customer_email = Column(String(255), ForeignKey('client.email'))
+    payment_type = Column(String(255))
+    amount = Column(Double, ForeignKey('membership.cost'))  
+    
+    #checks what sort of payment it is and therfore were to pull the value from
+    def switch(payment_type):
+            
+        if payment_type == "membership":
+            amount = Column(Double, ForeignKey('proffesionalservicerequest.cost'))  
+            profesional_id = Column(Integer, ForeignKey('professional.id'))
+            transaction_id = Column(Integer, ForeignKey('transaction.id'))                  
+
+    def __init__(self, payment_id, client_id, customer_email, amount, payment_type, profesional_id, transaction_id):
+        self.payment_id = payment_id
+        self.client_id = client_id
+        self.customer_email = customer_email
+        self.amount = amount
+        self.payment_type = payment_type
+        
+        # expected behaviour is for it to determin if its a subscription payment and if 
+        # so finish the initiliation here 
+        # honestly dont even know if this works
+        def switch(payment_type):
+            
+            if payment_type == "membership":
+                return
+        
+        self.profesional_id = profesional_id
+        self.transaction_id = transaction_id   
+        
