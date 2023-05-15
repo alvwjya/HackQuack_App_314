da45554e5f7a89da77d259a319d8e3b6759570bf
zayyehtut <zayyehtut.zy@gmail.com>
2023-05-15 15:41:19 +1000

    Update and rename EditProfile.py to EditProfessional.py

--- backend/controller/EditProfessional.py
+++ backend/controller/EditProfessional.py
@@ -1,19 +1,20 @@
+# Import necessary modules and objects
 from models import Professional
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from ..flaskConfig import app
+from flaskConfig import app
 
 from sqlalchemy.orm import sessionmaker
 from database import Base, engine
 from datetime import datetime, timedelta
 import re
 
+# Set up a sessionmaker bound to the engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
-CORS(app)
-
+# Define a function to update a professional's profile
 def update_professional_profile(professional_id, first_name, last_name, abn, email, business_name, suburb, state, postcode):
     professional = session.query(Professional).get(professional_id)
     if not professional:
@@ -60,6 +61,7 @@ def update_professional_profile(professional_id, first_name, last_name, abn, ema
 
     return {"status": "success", "message": "Profile updated successfully"}
 
+# Define a route to handle update requests
 @app.route("/update-professional", methods=["POST"])
 @cross_origin()
 def updateProfessional():
@@ -77,6 +79,7 @@ def updateProfessional():
     result = update_professional_profile(professional_id, first_name, last_name, abn, email, business_name, suburb, state, postcode)
     return jsonify(result)
 
+# Define a function to record failed update attempts
 def record_failed_update(professional_id):
     professional = session.query(Professional).get(professional_id)
     if not professional:
@@ -101,6 +104,7 @@ def record_failed_update(professional_id):
 
     return {"status": "success", "message": "Failed update recorded"}
 
+# Define a route to handle failed update recording requests
 @app.route("/record-failed-update", methods=["POST"])
 @cross_origin()
 def recordFailedUpdate():
