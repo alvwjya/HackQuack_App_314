48109f7a3c635b9fdc1183dcdf737513be3873b0
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-15 13:14:44 +1000

    Update

--- backend/controller/ProfessionalAcceptRequest.py
+++ backend/controller/ProfessionalAcceptRequest.py
@@ -9,7 +9,9 @@ from database import session_factory
 @app.route('/professionalAcceptRequest/<int:id>', methods=['PUT'])
 def professional_accept_request(id):
     session = session_factory()
-    data = request.get_json()
     professionalServiceRequest = session.query(professionalServiceRequest).get(id)
-    
+    if not professionalServiceRequest:
+        return {"status": "error", "message": "Professional service request not found"}
+    data = request.get_json()
+    professionalServiceRequest.cost = data['cost']
     return jsonify({'message': 'Professional accept request successfully!'})
