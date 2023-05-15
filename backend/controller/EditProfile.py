3bba1200005736f39d6c011857afe848df2a81f6
zayyehtut <zayyehtut.zy@gmail.com>
2023-05-15 15:11:19 +1000

    Update EditProfile.py

--- backend/controller/EditProfile.py
+++ backend/controller/EditProfile.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from flaskConfig import app
 
 from sqlalchemy.orm import sessionmaker
 from database import Base, engine
