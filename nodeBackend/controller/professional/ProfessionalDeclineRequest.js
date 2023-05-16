09cc9b9ab27468f0fed8ec1eaced186c4c1eca41
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-16 18:15:50 +1000

    Update ProfessionalDeclineRequest.js

--- nodeBackend/controller/professional/ProfessionalDeclineRequest.js
+++ nodeBackend/controller/professional/ProfessionalDeclineRequest.js
@@ -1,27 +1,34 @@
 // Front End: ProfessionalServiceBoardRequest
 
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const router = express.Router();
 
-
 router.post("/professional-decline-request", async function (req, res) {
-    const { id, acceptance, cost, service_request_id, professional_id, service_request, professional, transaction} = req.body;
-    const table = await prisma.professional_service_request.create({
-      data: {
-        id,
-        acceptance,
-        cost,
-        service_request_id,
-        professional_id,
-        service_request,
-        professional,
-        transaction,
-      },
-    });
-    res.json(table);
-  }
-);
+  const {
+    id,
+    acceptance,
+    cost,
+    service_request_id,
+    professional_id,
+    service_request,
+    professional,
+    transaction,
+  } = req.body;
+  const table = await prisma.professional_service_request.create({
+    data: {
+      id,
+      acceptance,
+      cost,
+      service_request_id,
+      professional_id,
+      service_request,
+      professional,
+      transaction,
+    },
+  });
+  res.json(table);
+});
 
 module.exports = router;
