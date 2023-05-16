a8a4d9818a970bc6ac808340e1f023603b690dc9
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-05-16 14:14:02 +1000

    push

--- nodeBackend/index.js
+++ nodeBackend/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const { PrismaClient } = require('@prisma/client');
 
-app.use(express.json())
-const prisma = new PrismaClient()
-
-app.get('/', (req, res) => {
-    console.log("Welcome to Hackquack");
-    res.send("Hello World!!!");
-})
+app.use(express.json());
+app.use(require("./controller/SignUp"))
+app.get("/", (req, res) => {
+  console.log("Welcome to Hackquack");
+  res.send("Hello World!!!");
+});
 
 // This is used to show the port of the server.
 app.listen(port, () => {
-    console.log("Server is running on port: ", port);
-})
+  console.log("Server is running on port: ", port);
+});
