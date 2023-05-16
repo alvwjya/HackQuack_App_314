caff18586f76f21b5fe8ced6887e33ebb0de78f6
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-05-16 18:05:28 +1000

    Update SignUp.js

--- nodeBackend/controller/SignUp.js
+++ nodeBackend/controller/SignUp.js
@@ -1,17 +1,42 @@
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post("/signup/professional", async function (req, res) {
-    const userId = req.params.userId;
-    let dailyAssess = {
-        breakfast: false,
-        lunch: false,
-        dinner: false,
-        snack: false
-    }
-    res.status(200).json({dailyAssess})
-})
+  const userId = req.params.userId;
+  const {
+    first_name,
+    last_name,
+    ABN,
+    TFN,
+    password,
+    suburb,
+    state,
+    postcode,
+    card_number,
+    card_security_num,
+    card_expiry_date,
+    service_type_id,
+  } = req.params.body;
+  const signupPeofessional = await prisma.professional.create({
+    data: {
+      first_name,
+      last_name,
+      ABN,
+      TFN,
+      password,
+      suburb,
+      state,
+      postcode,
+      card_number,
+      card_security_num,
+      card_expiry_date,
+      service_type_id,
+    },
+  });
+
+  res.status(200).json({ dailyAssess });
+});
 
 module.exports = router;
