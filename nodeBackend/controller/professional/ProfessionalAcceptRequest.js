eba3ef6863c155ca6e14bb9fbc05baa5f9220af0
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-16 18:05:05 +1000

    Update ProfessionalAcceptRequest.js

--- nodeBackend/controller/professional/ProfessionalAcceptRequest.js
+++ nodeBackend/controller/professional/ProfessionalAcceptRequest.js
@@ -5,30 +5,6 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post("/professional-accept-request", async function (req, res) {
-  const {
-    id,
-    acceptance,
-    cost,
-    service_request_id,
-    professional_id,
-    service_request,
-    professional,
-    transaction,
-  } = req.body;
-  const table = await prisma.professional_service_request.create({
-    data: {
-      id,
-      acceptance,
-      cost,
-      service_request_id,
-      professional_id,
-      service_request,
-      professional,
-      transaction,
-    },
-  });
-  res.json(table);
-});
+router.post("/professional-accept-request", async function (req, res) {});
 
 module.exports = router;
