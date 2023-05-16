5af35137aea6d5c9a4cc14fd373a43bcdb245942
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-16 17:06:40 +1000

    .

--- nodeBackend/controller/professional/ProfessionalAcceptRequest.js
+++ nodeBackend/controller/professional/ProfessionalAcceptRequest.js
@@ -6,10 +6,22 @@ const prisma = new PrismaClient()
 const router = express.Router();
 
 
-router.post("/professional-accept-request",async function (req, res) 
-{
-    
-}
+router.post("/professional-accept-request", async function (req, res) {
+    const { id, acceptance, cost, service_request_id, professional_id, service_request, professional, transaction} = req.body;
+    const table = await prisma.professional_service_request.create({
+      data: {
+        id,
+        acceptance,
+        cost,
+        service_request_id,
+        professional_id,
+        service_request,
+        professional,
+        transaction,
+      },
+    });
+    res.json(table);
+  }
 );
 
 module.exports = router;
--- nodeBackend/controller/professional/ProfessionalDeclineRequest.js
+++ nodeBackend/controller/professional/ProfessionalDeclineRequest.js
@@ -6,9 +6,22 @@ const prisma = new PrismaClient()
 const router = express.Router();
 
 
-router.post("/professional-decline-request",async function (req, res) {
-
-    }
+router.post("/professional-decline-request", async function (req, res) {
+    const { id, acceptance, cost, service_request_id, professional_id, service_request, professional, transaction} = req.body;
+    const table = await prisma.professional_service_request.create({
+      data: {
+        id,
+        acceptance,
+        cost,
+        service_request_id,
+        professional_id,
+        service_request,
+        professional,
+        transaction,
+      },
+    });
+    res.json(table);
+  }
 );
 
 module.exports = router;
