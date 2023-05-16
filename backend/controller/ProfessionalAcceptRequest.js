31b808ddf0dd2c2ed190ab1c3914743f88a32cf4
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-16 13:03:51 +1000

    .

--- backend/controller/ProfessionalAcceptRequest.js
+++ backend/controller/ProfessionalAcceptRequest.js
@@ -7,7 +7,7 @@ from flask_cors import cross_origin, CORS
 from database import session_factory
 
 @app.route('/professionalAcceptRequest/<int:id>', methods=['PUT'])
-def professional_accept_request(id):
+function professional_accept_request(id):
     session = session_factory()
     professionalServiceRequest = session.query(professionalServiceRequest).get(id)
     if not professionalServiceRequest:
