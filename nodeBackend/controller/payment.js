222c5fcf8b183a5a21cb151385478bd21c98563e
MadMonkMash <111272801+MadMonkMash@users.noreply.github.com>
2023-05-17 10:59:02 +1000

    small updates
    just have added some comments to the file

--- nodeBackend/controller/payment.js
+++ nodeBackend/controller/payment.js
@@ -22,18 +22,31 @@ router.post("/payment", async function(req, res){
     res.json(table);
 });
 
+/**
+ * A function to be called every time the server goes to the next day
+ * The only thing it does is get check to see if the due date for the membership payment is due
+ * pushes a new entry into the payment table then sets the due date to when it is next due
+*/
+
 function membershippayment(params) {
 
     
 
 }
-        
+
+/**
+ * is meant to be called at the end of a transaction function call and does the same as
+ * mebership payment but also pushes the information for the proffesional involved as well 
+ */        
 function jobPayment(Client, Professional) {
         
 
 
 }
-        
+
+/**
+ * A function to find users who membership is due today
+ */
 async function findUserName() {
  
     var today = Date();
