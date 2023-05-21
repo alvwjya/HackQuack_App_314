1f0e011e64285300d04b982f7fc5f1b0a5890868
nbadran <nabeelbadran7@gmail.com>
2023-05-21 13:32:25 +1000

    client-new-request (updated)

--- nodeBackend/controller/ClientNewRequest.js
+++ nodeBackend/controller/ClientNewRequest.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 const router = express.Router();
@@ -7,18 +7,24 @@ router.post("/client-new-request", async function(req, res){
     const{
         id,
         client_id,
-        professional_request_id,
+        request_title,
+        service_type_id,
         description
     } = req.body;
-    const table = await prisma.customer_new_request.create({
+
+    const clientNewRequest = await prisma.customer_new_request.create({
         data:{
             id,
             client_id,
-            professional_request_id,
+            professional_service_request: { connect: { professional_request_id: professional_request_id } },
+            request_title,
+            service_type_id,
+            request_time : new Date(),
             description
             },
-    });    
-    res.json(table);
+    });
+
+    res.json(clientNewRequest);
 });
 
 module.exports = router;
