b2e354b0d2f2825bfd89ed51bc95882090aa872c
AnnieAnNguyen <annie.annguyentran@gmail.com>
2023-05-23 01:36:18 +1000

    Update CustomerRatingAndReview.jsx

--- front-end/src/pages/customer/CustomerRatingAndReview.jsx
+++ front-end/src/pages/customer/CustomerRatingAndReview.jsx
@@ -1,45 +1,51 @@
 import React from "react";
-import { Container, Button, Form } from "react-bootstrap";
+import { Container, Button, Form, Card } from "react-bootstrap";
 import axios from "axios";
 
 function CustomerRatingAndReview() {
   const service_type = ["One", "Two", "Three", "Four", "Five"];
   return (
     <Container className="py-5">
-      <Form>
-        <h1>Rating and Review</h1>
+      <div class="container py-3">
+        <Card>
+          <Card.Body>
+            <h1>Rating and Review</h1>
 
-        <hr />
+            <hr />
 
-        <Form.Group className="mb-3" controlId="formBasicServiceType">
-          <Form.Label>Rating</Form.Label>
-          <Form.Select aria-label="rating">
-            <option value="">Please rate the service</option>
-            {service_type.map((item, index) => {
-              return <option value={index}>{item}</option>;
-            })}
-          </Form.Select>
-        </Form.Group>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicServiceType">
+                <Form.Label>Rating</Form.Label>
+                <Form.Select aria-label="rating">
+                  <option value="">Please rate the service</option>
+                  {service_type.map((item, index) => {
+                    return <option value={index}>{item}</option>;
+                  })}
+                </Form.Select>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicTaskDescription">
-          <Form.Label>Review</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Please write the review for the service"
-          />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicTaskDescription">
+                <Form.Label>Review</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Please write the review for the service"
+                />
+              </Form.Group>
+            </Form>
 
-        <hr />
+            <hr />
 
-        <div className="d-grid gap-2">
-          <Button className="btn-primary" size="lg">
-            Submit
-          </Button>
-          <Button href="/" className="btn-cancel" size="lg">
-            Cancel
-          </Button>
-        </div>
-      </Form>
+            <div className="d-grid gap-2">
+              <Button className="btn-primary" size="lg">
+                Submit
+              </Button>
+              <Button href="/" className="btn-cancel" size="lg">
+                Cancel
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     </Container>
   );
 }
