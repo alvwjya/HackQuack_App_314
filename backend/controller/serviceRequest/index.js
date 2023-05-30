f699b8fb64ac3d2e1c8b6b11acb1e55011675362
Alvian Wijaya <alvianwijaya200@gmail.com>
2023-05-31 08:45:55 +1000

    update customer view professional offers

--- backend/controller/serviceRequest/index.js
+++ backend/controller/serviceRequest/index.js
@@ -5,10 +5,12 @@ const clientNewRequest = require("./Client.NewRequest");
 const professionalNewOffer = require("./Professional.NewOffer");
 const clientGetAllProfessionalOffer = require("./Client.GetAllProfessionalOffer");
 const professionalGetClientRequest = require("./Professional.GetClientRequest");
+const getAllClientRequests = require("./Client.GetAllRequest");
 
 router.use("/client", clientNewRequest);
 router.use("/professional", professionalNewOffer);
 router.use("/client", clientGetAllProfessionalOffer);
 router.use("/professional", professionalGetClientRequest);
+router.use("/client", getAllClientRequests);
 
 module.exports = router;
